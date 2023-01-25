89ef40457b666ecd208e36dc1b0d79ce64baa937
Tabunov Egor <Vonubat@gmail.com>
2023-01-25 23:55:08 +0300

    feat: implement drawCircle method

--- src/ws_server/controller/index.ts
+++ src/ws_server/controller/index.ts
@@ -1,4 +1,4 @@
-import { mouse, up, down, left, right } from '@nut-tree/nut-js';
+import { mouse, up, down, left, right, straightTo, Button } from '@nut-tree/nut-js';
 import { Commands } from '../../constants';
 import { Controller } from '../types';
 
@@ -19,14 +19,26 @@ export const controller: Controller = {
     mouse.move(right(offset));
   },
 
-  [Commands.mousePosition]: async (/* TODO */): Promise<string> => {
+  [Commands.mousePosition]: async (): Promise<string> => {
     const { x, y } = await mouse.getPosition();
     const response: string = `${x},${y}`;
     return response;
   },
 
-  [Commands.drawCircle]: async (/* TODO */) => {
-    // TODO
+  [Commands.drawCircle]: async ([radius]: number[]): Promise<void> => {
+    const TURN = 360;
+    const { PI, cos, sin } = Math;
+    const { x, y } = await mouse.getPosition();
+
+    await mouse.pressButton(Button.LEFT);
+    for (let deg = 0; deg <= TURN; deg++) {
+      const rad = (PI / 180) * deg;
+      const xPoint = x - radius + radius * cos(rad);
+      const yPoint = y + radius * sin(rad);
+
+      await mouse.move(straightTo({ x: xPoint, y: yPoint }));
+    }
+    await mouse.releaseButton(Button.LEFT);
   },
 
   [Commands.drawSquare]: async (/* TODO */) => {
