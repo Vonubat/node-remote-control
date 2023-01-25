641ea9d0b0963e6423e6f2edfba9e290c99286ca
Tabunov Egor <Vonubat@gmail.com>
2023-01-25 16:21:10 +0300

    feat: add default PORTS

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_HTTP_PORT = 8181;
+export const DEFAULT_WS_PORT = 8080;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config';
+import { DEFAULT_HTTP_PORT, DEFAULT_WS_PORT } from './constants';
 import { httpServer } from './http_server/index';
-import { mouse } from '@nut-tree/nut-js';
+// import { mouse } from '@nut-tree/nut-js';
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = Number(process.env.HTTP_PORT) || DEFAULT_HTTP_PORT;
+const WS_PORT = Number(process.env.WS_PORT) || DEFAULT_WS_PORT;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
